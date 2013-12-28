654c19d23a78372a8ef4164e7a73f9ee2bd10a2f
Ian Serlin <ian@ianserlin.com>
2013-12-27 17:24:51 -0800

    adding tracking codes

---
+++ client/views/layout.js
@@ -0,0 +1,17 @@
+Template.layout.rendered = function(){
+	// GA
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35793772-1', 'ianserlin.com');
+  ga('send', 'pageview');
+
+  // CrazyEgg
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0013/8966.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+
+};
